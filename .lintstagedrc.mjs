cb189e3ca5376224aade8b8d9d6046e8051bb30c
colline-kooza <koozacollinz1@gmail.com>
2026-01-07 16:00:15 +0300

    initial commit

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 import path from "path";
 
-// See https://nextjs.org/docs/app/api-reference/config/eslint#running-lint-on-staged-files for details
+
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
